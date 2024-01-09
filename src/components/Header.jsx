86cdeb583a460bdaca8a57d4eaf5fda741522946
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2024-01-09 10:59:06 +0300

    button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import logo from "../assets/logo.jpg"
-export default function Header(){
-    return <header id="main-header">
-        <div id="title">
-            <img src={logo} alt="a restaurant" />
-            <h1>ReactFood</h1>
-        </div>
-        <nav><button>
-            Cart(0)
-        </button></nav>
+import logo from "../assets/logo.jpg";
+import Button from "./UI/Button";
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="a restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart(0)</Button>
+      </nav>
     </header>
+  );
 }
